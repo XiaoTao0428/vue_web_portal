3ccb8b20fce80a4b26c0f3d0be9badd1c059c286
YuanTao <taoyuan0428@gmail.com>
2021-12-13 20:11:28 +0800

    新增了路径别名；新增了element ui；新增了中英国际化；

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import i18n from './i18n/i18n';
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
