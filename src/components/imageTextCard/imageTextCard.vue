8e8bd10c6fa9afa3697ebd7027c72693e63b8e02
Administrator <645329134@qq.com>
2021-12-20 16:54:28 +0800

    添加了图文卡片组件，添加了页面头部块

--- src/components/imageTextCard/imageTextCard.vue
+++ src/components/imageTextCard/imageTextCard.vue
@@ -1,20 +1,88 @@
 <template>
   <div class="imageTextCard_warp">
-
+    <div class="image-description">
+      <img class="image" :src="imageUrl">
+      <div class="line"></div>
+    </div>
+    <div class="date">
+      <i class="el-icon-menu"></i>
+      <span>{{date}}</span>
+    </div>
+    <div class="description">
+      {{description}}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "imageTextCard",
+  props: {
+    /**
+     * 图片地址
+     * */
+    imageUrl: {
+      type: Text,
+      default: 'https://caltechsites-prod.s3.amazonaws.com/root/images/Lasers.2e16d0ba.fill-710x400-c100.jpg',
+    },
+    /**
+     * 日期
+     * */
+    date: {
+      type: Text,
+      default: '2020年6月18日'
+    },
+    /**
+     * 描述
+     * */
+    description: {
+      type: Text,
+      default: '光学微梳装置可能会改善电信、传感器、时钟',
+    }
+  },
   data() {
-    return {}
+    return {
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .imageTextCard_warp {
+  width: 100%;
 
+  .image-description {
+    width: 100%;
+    margin-bottom: 20px;
+    .image {
+      max-width: 100%;
+      margin-bottom: -4px;
+      object-fit: contain;
+    }
+    .line {
+      width: 100%;
+      height: 4px;
+      background-image: linear-gradient(90deg, #f9be01 0%, #c75000 100%);
+    }
+  }
+
+  .date {
+    font-size: 12px;
+    color: #D14900;
+    font-weight: bold;
+    margin-bottom: 10px;
+
+    .el-icon-menu {
+      font-size: 16px;
+      color: #AAA99F;
+      margin-right: 5px;
+    }
+  }
+
+  .description {
+    font-size: 22px;
+    color: #000000;
+    font-weight: bold;
+  }
 }
 </style>
---
+++ src/components/pageHeaderBlock/pageHeaderBlock.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="pageHeaderBlock_warp">
+    <el-breadcrumb class="breadcrumb" separator="/">
+      <template v-for="(item, index) in breadcrumbList" :key="'breadcrumbList' + index">
+        <el-breadcrumb-item>
+          <a v-if="item.to" :href="item.to">{{item.title}}</a>
+          <span v-else>{{item.title}}</span>
+        </el-breadcrumb-item>
+      </template>
+    </el-breadcrumb>
+    <div class="title">
+      {{title}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pageHeaderBlock",
+  props: {
+    /**
+    * 标题
+    * */
+    title: {
+      type: Text,
+      default: '标题',
+    },
+    breadcrumbList: {
+      type: Array,
+      default: () => [
+        {
+          title: '首页',
+          to: '/Home',
+        },
+        {
+          title: '研究',
+          to: '/Research',
+        },
+        {
+          title: '人们',
+          to: '/People',
+        }
+      ]
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.pageHeaderBlock_warp {
+  width: 100%;
+  height: 160px;
+  background-image: linear-gradient(-226deg, #003b4c 0%, #005851 100%);
+  //padding-left: 50px;
+  //padding-right: 50px;
+  padding: 30px 130px;
+  box-sizing: border-box;
+  margin-bottom: 30px;
+  .breadcrumb {
+    font-size: 14px;
+    color: #ffffff;
+  }
+  .title {
+    color: #ffffff;
+    font-size: 50px;
+  }
+}
+</style>
