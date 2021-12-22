56d1bb45e208d90d5feda66e6d3cc95d42a90ba3
Administrator <645329134@qq.com>
2021-12-22 11:26:29 +0800

    调整了页面内容配置项

--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -1,3 +1,6 @@
 // 接口域名
 
 export const BASE_URL = 'http://192.168.0.24:8000'
+
+// 上传文件的地址
+export const upload_file_URL = 'https://jsonplaceholder.typicode.com/posts/'
--- src/views/pageContentManage/pageContentManage.vue
+++ src/views/pageContentManage/pageContentManage.vue
@@ -5,7 +5,63 @@
 
         <div class="tab-pane-content">
 
-          <modify-paper-content></modify-paper-content>
+<!--          <modify-paper-content></modify-paper-content>-->
+
+          <div class="card home-image">
+            <div class="card-title">
+              1、首页右侧主图
+            </div>
+            <div class="card-content">
+              <el-form ref="homeImageForm" :model="homeImageForm" label-width="80px">
+                <el-form-item label="图片描述">
+                  <el-input type="textarea" v-model="homeImageForm.describe" :rows="3"></el-input>
+                </el-form-item>
+                <el-form-item label="图片上传">
+                  <el-upload
+                      :action="uploadAction"
+                      :headers="uploadHeaders"
+                      :multiple="false"
+                      name="image"
+                      :limit="1"
+                      list-type="picture-card"
+                      :on-exceed="handleImageUploadExceed"
+                      :on-preview="handleImageUploadPreview"
+                      :before-upload="beforeImageUpload"
+                      :on-remove="handleImageUploadRemove"
+                      :on-success="handleImageUploadSuccess"
+                  >
+                    <img v-if="homeImageForm.imageUrl" :src="homeImageForm.imageUrl" class="avatar">
+                    <i  v-if="!homeImageForm.imageUrl" class="el-icon-plus avatar-uploader-icon"></i>
+                    <div class="el-upload__tip" slot="tip">最大允许上传个数为1，只能上传jpg/png文件，且不超过2MB</div>
+                  </el-upload>
+                </el-form-item>
+              </el-form>
+
+              <div class="btn-warp">
+                <el-button>提 交</el-button>
+              </div>
+
+              <el-dialog :visible.sync="previewDialogVisible">
+                <img width="100%" :src="previewImageUrl" alt="">
+              </el-dialog>
+
+            </div>
+          </div>
+
+          <div class="card home-paper">
+            <div class="card-title">
+              2、首页左侧文章
+            </div>
+
+            <mavon-editor v-model="homePaperValue"
+                          :language="'zh-CN'"
+            ></mavon-editor>
+
+            <div class="btn-warp">
+              <el-button>提 交</el-button>
+            </div>
+
+          </div>
 
         </div>
 
@@ -21,17 +77,60 @@
 
 <script>
 import ModifyPaperContent from "@/components/modifyPaperContent/modifyPaperContent";
+import {upload_file_URL} from '@/config/baseURL'
 export default {
   name: "pageContentManage",
   components: {ModifyPaperContent},
   data() {
     return {
+      uploadAction: upload_file_URL,
+      uploadHeaders: {},
       activeName: 'first',
+      homeImageForm: {
+        describe: '',
+        imageUrl: '',
+      },
+      previewDialogVisible: false,
+      previewImageUrl: '',
+      homePaperValue: '**二万人微软为*微软 微软++ 微软++***',
     }
   },
   methods: {
     handleTabClick(tab) {
+      console.log(tab)
+    },
+    beforeImageUpload(file) {
+      const fileType = file.type
+      const isLt2M = file.size / 1024 / 1024 <= 2;
 
+      let isJPGOrPng = true
+      if (fileType !== 'image/jpeg' && fileType !== 'image/png') {
+        this.$message.error('上传图片只能是 JPG/JPEG 或 PNG 格式!')
+        isJPGOrPng = false
+      }
+      if (!isLt2M) {
+        this.$message.error('上传图片大小不能超过 2MB!')
+      }
+
+      if (isJPGOrPng && isLt2M) {
+
+      }
+
+      return isJPGOrPng && isLt2M
+    },
+    handleImageUploadExceed(files, fileList) {
+      this.$message.warning('超出了数量上限！')
+    },
+    handleImageUploadPreview(file) {
+      console.log(file.url)
+      this.previewImageUrl = file.url
+      this.previewDialogVisible = true
+    },
+    handleImageUploadRemove(file, fileList) {
+      this.homeImageForm.imageUrl = ''
+    },
+    handleImageUploadSuccess(res, file) {
+      console.log('handleImageUploadSuccess', res)
     },
   }
 }
@@ -49,6 +148,26 @@ export default {
 
       .tab-pane-content {
         width: 100%;
+
+        .card {
+          width: 100%;
+          &:first-child {
+            margin-bottom: 40px;
+          }
+
+          .card-title {
+            font-size: 20px;
+            margin-bottom: 20px;
+          }
+          .card-content {
+            width: 100%;
+          }
+        }
+        .home-paper {
+          .btn-warp {
+            margin-top: 20px;
+          }
+        }
       }
     }
   }
